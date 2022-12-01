f6808a6e1df94d00012637a54fbb69f709b7f4f4
Emre <emre.bahar35@outlook.com>
2022-12-01 22:07:55 +0300

    s

---
+++ www.patika.dev/opertörler/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opertörler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atama ve işlem atama 
+
+            int x = 3;
+            int y = 5;
+            y = y + 2;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+            y *= 10;
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+            y /= 1;
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+            x *= 6;
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+            //Mantıksal opertörler
+
+            bool ısSuccessful=true;
+            bool ısCompleted=false;
+
+            if (ısSuccessful && ısSuccessful)
+            {
+                Console.WriteLine("Perfect");
+
+            }
+            if (ısSuccessful || ısCompleted)
+            {
+                Console.WriteLine("Great");
+            }
+
+            if (ısSuccessful&& !ısCompleted)
+            {
+                Console.WriteLine("Final");
+            }
+        
+            Console.ReadLine();
+
+            //İlişkisel opretörler
+
+            int a = 20;
+            int b = 10;
+            bool sonuc = a < b;
+
+            sonuc= a > b;
+            Console.WriteLine(sonuc);
+
+            sonuc= a>b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a<b;
+            Console.WriteLine(sonuc);
+
+            Console.ReadLine();
+
+            int sayı1 = 10;
+            int sayı2 = 5;
+            int sonuc1 = sayı1 / sayı2;
+            Console.WriteLine(sonuc1);
+
+            int sonuc2 = sayı1 * sayı2;
+            Console.WriteLine(sonuc2);
+
+            sonuc1 =sayı2 + sayı2;
+            Console.WriteLine(sonuc1);
+
+            sonuc2 = sayı2 / sayı1;
+            Console.WriteLine(sonuc2);
+
+            Console.WriteLine("İşlem bitti");
+            Console.ReadLine();
+
+            //Mod alma
+
+             sonuc1 =20%3 ;
+            Console.WriteLine(sonuc1);
+       
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ www.patika.dev/patilaDev/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace patilaDev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            byte a = 8;
+            Console.WriteLine(a);
+
+            sbyte b = 12;
+            Console.WriteLine(b);
+
+            short c = 400;
+            Console.WriteLine(c);
+
+            ushort d = 10142;
+            Console.WriteLine(d);
+
+            int e = 788;
+            Console.WriteLine(e);
+
+            long f = 96542;
+            Console.WriteLine(f);
+
+            float g =45.8554f;
+            Console.WriteLine(g);
+
+            double h = 9.548785;
+            Console.WriteLine(h);
+
+            string name_surmane = "Odesia";
+            Console.WriteLine(name_surmane);
+
+
+            bool e1 = 10 > 4;
+            Console.WriteLine(e1);
+
+            bool r1 = 2 > 7;
+            Console.WriteLine(r1);
+
+            DateTime dtt = DateTime.Now;
+            Console.WriteLine(dtt);
+
+            string dataTime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine("Date:" +dataTime);
+
+            string hour = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine("Hour:"+hour);
+
+
+
+            object i5 = 13;
+            Console.WriteLine(i5);
+
+            object d3 = 23;
+            Console.WriteLine(d3);
+
+            object s2 = 13245743353;
+            Console.WriteLine(s2);
+
+            object g4 = "Mozart Lacrimosa";
+            Console.WriteLine(g4);
+
+
+            string str2 = string.Empty;
+            str2 = "Titan";
+            Console.WriteLine(str2); 
+            
+            int num1 = 6;
+            int num2 = 8;
+
+            Console.WriteLine(num2+num2);
+
+            Console.WriteLine(num1*num2);
+
+            Console.WriteLine(num1/num2);
+
+
+            string num3 = "234";
+            int num4 = 221;
+            string newString = num3 + num4.ToString();
+            int newInt = Convert.ToInt32(num3) + num4;
+
+            Console.WriteLine("int>new number="+newInt);
+
+        }
+    }
+}
